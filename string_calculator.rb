945e7e8d5b69c1d33205110d1ea7c7fb356b9dee
Hari Prakash <hariprakash002@gmail.com>
2025-10-27 21:04:50 +0530

    handle multiple numbers and delimeters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,23 @@
+def parse_delimiter(numbers)
+  if numbers.start_with?("//")
+    delimiter_line, numbers = numbers.split("\n", 2)
+    delimiter = delimiter_line[2..-1]
+    raise "Invalid delimiter" if delimiter.empty?
+    [delimiter, numbers]
+  else
+    [",", numbers]
+  end
+end
+
 def add(numbers)
   return 0 if numbers.empty?
+
   begin
-    numbers.split(",").map { |num| num.to_i }.sum
+    delimiter, numbers = parse_delimiter(numbers)
+    numbers.gsub("\n", delimiter).split(delimiter).reject(&:empty?).map { |num| num.to_i }.sum
   rescue ArgumentError
     raise "Invalid input: numbers must be integers"
+  rescue TypeError
+    raise "Invalid delimiter format"
   end
 end
