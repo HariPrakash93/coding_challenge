65e76cd1f511cfb899ff8416dc12b45225926027
Hari Prakash <hariprakash002@gmail.com>
2025-10-27 21:16:23 +0530

    handle exception and multiple numbers

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -17,4 +17,24 @@ class TestStringCalculator < Minitest::Test
   def test_multiple_numbers
     assert_equal 10, add("1,2,3,4")
   end
+
+  def test_newline_delimiter
+    assert_equal 6, add("1\n2,3")
+  end
+
+  def test_custom_delimiter
+    assert_equal 3, add("//;\n1;2")
+  end
+
+  def test_negative_number
+    assert_raises(RuntimeError, "negative numbers not allowed -2") do
+      add("1,-2")
+    end
+  end
+
+  def test_multiple_negative_numbers
+    assert_raises(RuntimeError, "negative numbers not allowed -2,-3") do
+      add("1,-2,-3,4")
+    end
+  end
 end
